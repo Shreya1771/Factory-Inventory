95c4761432a09935189229b01c0f746641742278
shreya <shreyasonawane54@gmail.com>
2017-05-29 20:53:38 +0530

    Create sales.java

---
+++ sales.java
@@ -0,0 +1,26 @@
+SALES PROGRAM:
+
+ 
+
+class sales
+
+{
+
+    long salescount;
+
+    double salesproductprice;
+
+    public sales(long salescount,double salesproductprice)
+
+    {
+
+        this.salescount=salescount;
+
+        this.salesproductprice=salesproductprice;
+
+    }
+
+}
+
+ 
+
