5ff8651498305759542c50be69d119e58d7b136d
shreya <shreyasonawane54@gmail.com>
2017-05-29 20:54:17 +0530

    Create product.java

---
+++ product.java
@@ -0,0 +1,30 @@
+PRODUCT PROGRAM:
+
+ 
+
+class product
+
+{
+
+    int pid;
+
+    double price;
+
+    int count;
+
+    public product(int pid,double price,int count)
+
+    {
+
+        this.pid=pid;
+
+        this.price=price;
+
+        this.count=count;
+
+    }
+
+}
+
+ 
+
