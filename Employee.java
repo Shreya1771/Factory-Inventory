99d9d71f28ee52e027311acbe9b59f7220f49082
shreya <shreyasonawane54@gmail.com>
2017-05-29 20:52:43 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,26 @@
+EMPLOYEE PROGRAM :
+
+ 
+
+class employee
+
+{
+
+    int eid;
+
+    double price;
+
+    public employee(int eid,double price)
+
+    {
+
+        this.eid=eid;
+
+        this.price=price;
+
+    }
+
+}
+
+ 
+
